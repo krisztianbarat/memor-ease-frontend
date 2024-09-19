e22875f98b90e136118a730f6fe661eaaa9fb46b
Krisztián Barát <krisztian.barat@sundroid.hu>
2024-09-19 20:45:33 +0200

    fix default content

--- apps/memor-ease-client/src/app/app.config.ts
+++ apps/memor-ease-client/src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    importProvidersFrom(
+      NgxsModule.forRoot([]),
+      NgxsLoggerPluginModule.forRoot(),
+      NgxsReduxDevtoolsPluginModule.forRoot()
+    )
   ],
 };
