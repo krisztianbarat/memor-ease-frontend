f775a961f1bd0fbf6a8e53badff450105984fc2e
Krisztián Barát <krisztian.barat@sundroid.hu>
2024-09-19 22:08:52 +0200

    parts of screen

--- apps/memor-ease-client/src/app/app.config.ts
+++ apps/memor-ease-client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
@@ -8,7 +8,7 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     importProvidersFrom(
       NgxsModule.forRoot([]),
       NgxsLoggerPluginModule.forRoot(),
--- apps/memor-ease-client/src/app/app.routes.ts
+++ apps/memor-ease-client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { of } from 'rxjs';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard-feature/pages/dashboard/dashboard.component').then(component => component.DashboardComponent)
+  }
+];
