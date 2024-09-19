8884b59b31b9e09805495a34aaf09416350f1a53
Krisztián Barát <krisztian.barat@sundroid.hu>
2024-09-19 20:58:32 +0200

    add app-main

--- apps/memor-ease-client/src/app/app.component.ts
+++ apps/memor-ease-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MainComponent } from './cores/components/main/main.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MainComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
